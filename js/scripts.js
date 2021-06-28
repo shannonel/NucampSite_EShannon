1de169f55d6f69fa6fdd85c8253dd749b01d4da1
shannonel <85324902+shannonel@users.noreply.github.com>
2021-06-27 23:18:21 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,11 @@ $(function(){
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     })
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
 });
